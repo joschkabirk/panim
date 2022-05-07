f470fc736372daab05b829e9a0b031a767f96d34
Joschka Birk <joschka.birk@gmail.com>
2022-05-07 12:56:58 +0200

    Adding sphinx docs that work locally

---
+++ docs/build_docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install requirements and panim
+pip install .
+
+# install requirements for sphinx
+pip install -r docs/requirements.txt
+
+# build the documentation
+cd docs
+rm -rf _build _static _templates
+sphinx-apidoc -f -o . ../panim
+make html
+
+# we have to create an empty .nojekyll file in order to make the html theme work
+touch _build/.nojekyll

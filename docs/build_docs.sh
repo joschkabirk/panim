575a9e09737a79720badcbcf63ca0d3e7dc53192
Joschka Birk <joschka.birk@gmail.com>
2022-05-25 09:29:25 +0200

    fix librep

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+# install requirements and puma
 pip install .
 
 # install requirements for sphinx
 pip install -r docs/requirements.txt
 
 # build the documentation
-cd docs
-rm -rf _build _static _templates
-# sphinx-build -b html source _build/html
-# mkdir -p _build/html
-# cp source/_static/versions.json _build/html
-sphinx-multiversion source _build/html
-# copy the index.html that redirects to the master folder
-cp source/redirect_index.html _build/html/index.html
+rm -rf docs/_*
+python docs/sphinx_build_multiversion.py
+# copy the redirect_index.html that redirects to the main/latest version
+cp docs/source/redirect_index.html docs/_build/html/index.html
 
 # we have to create an empty .nojekyll file in order to make the html theme work
-touch _build/html/.nojekyll
+touch docs/_build/html/.nojekyll

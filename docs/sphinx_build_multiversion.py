b919ea5bde55e6db7077072c7efc410bdb933d2e
Joschka Birk <joschka.birk@gmail.com>
2023-08-03 23:15:41 +0200

    return to capture_output=True

--- docs/sphinx_build_multiversion.py
+++ docs/sphinx_build_multiversion.py
@@ -56,7 +56,7 @@ def main():
 
     # get currently active branch
     command = "git rev-parse --abbrev-ref HEAD".split()
-    initial_branch = run(command, capture_output=False, check=True).stdout.strip().decode("utf-8")
+    initial_branch = run(command, capture_output=True, check=True).stdout.strip().decode("utf-8")
     
     copy("docs/source/conf.py", "./conf_latest.py")
 
